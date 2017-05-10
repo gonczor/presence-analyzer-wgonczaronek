4e69492d686e78be2f5507df8e8bcc47d6ba56e4
gonczor <wiktor.gonczaronek@stxnext.pl>
2017-05-10 07:58:40 +0200

    Bug fixed

--- src/presence_analyzer/static/js/get_avatar.js
+++ src/presence_analyzer/static/js/get_avatar.js
@@ -3,18 +3,19 @@
         $('#user_id').change(function() {
             var getAvatarUrl = '/api/v1/user_avatar_url/' + $('#user_id').val(),
                 $userAvatarElement = $('#user_avatar'),
-                intranetBaseUrl = 'https://intranet.stxnext.pl';
+                intranetBaseUrl = 'https://intranet.stxnext.pl',
+                $errorSection = $('#error-message');
 
             $.getJSON(getAvatarUrl, function(result) {
+                $errorSection.text('');
                 $userAvatarElement.attr('src', intranetBaseUrl + result);
             }).fail(function() {
-                var $errorSection = $('#error-message'),
-                    $selectedUser = $('#user_id').val();
+                var $selectedUser = $('#user_id').val();
 
+                $userAvatarElement.attr('src', '');
                 if($selectedUser) {
-                    $userAvatarElement.attr('src', '');
                     $errorSection.text('User details not found.');
-                } else{
+                } else {
                     $errorSection.text('');
                 }
             });
